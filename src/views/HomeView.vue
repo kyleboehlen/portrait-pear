7ab30c033d46b9a513605000da5866d3225b545c
Kyle Boehlen <kyleboehlen@gmail.com>
2022-12-29 11:01:42 -0700

    Configure Capacitor for iOS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,31 @@
 </template>
 
 <script setup>
+// Axios
+import axios from "axios"
+// Icons
 import { Icon, addIcon } from "@iconify/vue/offline"
 import camera from "@iconify-icons/material-symbols/camera"
+// Capacitor
+import { SplashScreen } from "@capacitor/splash-screen"
+// Vue
+import { onMounted } from "vue"
 
 addIcon("camera", camera)
+
+onMounted(() => {
+  axios({
+    method: "get",
+    url: `http://localhost/api/pear/home`,
+  })
+    .then(function (response) {
+      console.log(response.data)
+      setTimeout(() => {
+        SplashScreen.hide()
+      }, 500)
+    })
+    .catch(function () {
+      console.log("failed")
+    })
+})
 </script>
