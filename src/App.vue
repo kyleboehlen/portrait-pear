9de8750dcbf4b1a32077075376a89c5d76e8411b
Kyle Boehlen <kyleboehlen@gmail.com>
2022-12-29 18:51:44 -0700

    Stick footer to bottom always - move splash screen hide to be the same regardless of api call length

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="app-wrapper flex flex-col min-h-screen justify-around">
+  <div class="app-wrapper flex flex-col min-h-screen justify-start">
     <TheHeader />
-    <RouterView />
-    <TheFooter />
+    <RouterView class="grow" />
+    <TheFooter class="justify-self-end" />
   </div>
 </template>
 
 <script setup>
+// Vue
+import { onMounted } from "vue"
 import { RouterView } from "vue-router"
+// Components
 import TheHeader from "@/components/TheHeader.vue"
 import TheFooter from "@/components/TheFooter.vue"
+// Capacitor
+import { SplashScreen } from "@capacitor/splash-screen"
+
+onMounted(() => {
+  setTimeout(() => {
+    SplashScreen.hide()
+  }, 750)
+})
 </script>
